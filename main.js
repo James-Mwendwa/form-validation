cf648e27ee4d6c1d133c36c2edfb6bb91cdf73ff
James Mwendwa <mwendwajames32@gmail.com>
2021-11-13 21:21:21 +0300

    files

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const confirmPassword = document.querySelector("#confirm-password");
 
 const signUp = document.querySelector('.btn');
 
+
+// prevent the form from submitting when clicked
+
+signUp.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
