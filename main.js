4f3ac2a32dd75cb1a945f83f66e289f14ec20e6a
James Mwendwa <mwendwajames32@gmail.com>
2021-11-13 21:14:12 +0300

    files

---
+++ main.js
@@ -0,0 +1,7 @@
+const username = document.querySelector('#username')
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector("#confirm-password");
+
+const signUp = document.querySelector('.btn');
+
